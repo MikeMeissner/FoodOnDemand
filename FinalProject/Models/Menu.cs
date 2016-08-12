abd8fa757783cabacdace0cf6e18cae67a535e1d
MikeMeissner <tennisapb@gmail.com>
2016-08-12 00:48:27 -0400

    little things

---
+++ FinalProject/Models/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Menu
+    {
+        public Menu()
+        {
+
+        }
+        public Menu(int foodID, string Name, double price, string shortDescription, string Description)
+        {
+            this.FoodID = FoodID;
+            this.Name = Name;
+            this.Price = Price;
+            this.shortDescription = shortDescription;
+            this.Description = Description;
+
+        }
+        public virtual int FoodID { get; set; }
+        public virtual string foodImageUrl { get; set; }
+        public virtual string Name { get; set; }
+        public virtual double Price { get; set; }
+        public virtual string shortDescription { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
---
+++ FinalProject/Models/MenuContext.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalProject.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public class MenuContext : DBContext
+    {
+        public MenuContext()
+            : base("name=MenuConnection")
+        {
+        }
+        public virtual DbSet<Menu> Menu { get; set; }
+    }
+}
