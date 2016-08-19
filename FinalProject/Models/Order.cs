ea0b77aac505a868a3eb8f379924a2e267144b1d
nathan-siu <nathansiu9730@gmail.com>
2016-08-19 16:10:47 -0400

    fixed the check out form

--- FinalProject/Models/Order.cs
+++ FinalProject/Models/Order.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 /**
  * Names: Nathan Siu and mike Meissner
  * File Name Order.cs
@@ -10,9 +12,12 @@ using System.Web;
  * */
 
 namespace FinalProject.Models
-{
+{   
+    [Bind(Exclude ="OrderId")]
     public class Order
     {
+        [ScaffoldColumn(false)]
+        
         public virtual int OrderId { get; set; }
         public virtual string Username { get; set; }
         public virtual string Firstname { get; set; }
@@ -24,7 +29,9 @@ namespace FinalProject.Models
         public virtual string Country { get; set; }
         public virtual string Phone { get; set; }
         public virtual string Email { get; set; }
+        [ScaffoldColumn(false)]
         public virtual decimal Total { get; set; }
+        [ScaffoldColumn(false)]
         public virtual System.DateTime OrderDate { get; set; }
         public virtual List<OrderDetail> OrderDetails { get; set; }
     }
