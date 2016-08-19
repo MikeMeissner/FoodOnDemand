2190fbc8a0cc7b1d043361678f605e576c9dc214
nathan-siu <nathansiu9730@gmail.com>
2016-08-19 15:43:55 -0400

    added images and font awesome icons

--- FinalProject/App_Start/BundleConfig.cs
+++ FinalProject/App_Start/BundleConfig.cs
@@ -30,6 +30,7 @@ namespace FinalProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
